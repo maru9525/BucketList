d4e01551028daa63683d55a92e05c0515989145a
maru9525 <cember123@gmail.com>
2022-01-26 15:55:00 +0900

    feat: Settings

---
+++ src/main/java/prototype/BucketList/BucketListApplication.java
@@ -0,0 +1,13 @@
+package prototype.BucketList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prototype/BucketList/BucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package prototype.BucketList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
